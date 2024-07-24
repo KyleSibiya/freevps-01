ee60260c0d85213b9f04f71ec1253a98d579fb03
KyleSibiya <sibiyambuso50@gmail.com>
2024-07-24 16:39:48 +0200

    Update make-executable.sh

--- make-executable.sh
+++ make-executable.sh
@@ -1,9 +1,9 @@
 # create the script you want to run 
-touch home/YOURUSERNAME/sample-script.sh
+touch home/KyleSibiya/sample-script.sh
 # make it executable
-sudo chmod u+x home/YOURUSERNAME/sample-script.sh
+sudo chmod u+x home/KyleSibiya/sample-script.sh
 
 # to make it run at startup drop it into rc.local or something 
 # inside rc.local 
-home/YOURUSERNAME/sample-script.sh
+home/KyleSibiya/sample-script.sh
 exit 0
