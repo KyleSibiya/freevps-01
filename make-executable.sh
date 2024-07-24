b242ece63d0936cbef5daded4c9f142a198ed10a
KyleSibiya <sibiyambuso50@gmail.com>
2024-07-24 16:36:45 +0200

    Add files via upload

--- make-executable.sh
+++ make-executable.sh
@@ -1,9 +1,9 @@
 # create the script you want to run 
-touch home/KyleSibiya/sample-script.sh
+touch home/YOURUSERNAME/sample-script.sh
 # make it executable
-sudo chmod u+x home/KyleSibiya/sample-script.sh
+sudo chmod u+x home/YOURUSERNAME/sample-script.sh
 
 # to make it run at startup drop it into rc.local or something 
 # inside rc.local 
-home/KyleSibiya/sample-script.sh
+home/YOURUSERNAME/sample-script.sh
 exit 0
